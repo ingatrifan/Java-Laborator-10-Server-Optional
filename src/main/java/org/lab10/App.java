2422ebf16012f7cdb35d17e2e95585cba296b251
ingatrifan <45402357+ingatrifan@users.noreply.github.com>
2020-05-06 15:01:56 +0300

    lil changes

--- src/main/java/org/lab10/App.java
+++ src/main/java/org/lab10/App.java
@@ -4,19 +4,10 @@ import GameLogic.Board;
 import GameLogic.HtmlBuilder;
 import GameLogic.WebServer;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
     {
-//        Board board = new Board();
-//        HtmlBuilder htmlBuilder = new HtmlBuilder(board);
-//        WebServer webServer = new WebServer();
-//        webServer.createConexion();
-//        webServer.sendHtml(htmlBuilder.getFilePath());
         GameServer gs = new GameServer();
     }
 }
